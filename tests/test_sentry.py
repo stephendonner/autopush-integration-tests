ee448b8934a1248dc8dbb67b0e410707fc4f07aa
Richard Pappalardo <rpappalax@gmail.com>
2017-08-15 15:37:30 -0700

    hack: try sentry api

--- tests/test_sentry.py
+++ tests/test_sentry.py
@@ -7,4 +7,10 @@ from raven import Client
 @pytest.mark.nondestructive
 def test_sentry_check():
     print(os.environ['URL_SENTRY'])
+    url_sentry = os.environ['URL_SENTRY']
+    client = Client(url_sentry)
+    try:
+        1/0
+    except ZeroDivisionError:
+        client.captureException()
     assert(True)
