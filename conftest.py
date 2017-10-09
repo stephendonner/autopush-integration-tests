6179ffdf3cfb13d61ec7b18ef96b776007509a05
Richard Pappalardo <rpappalax@gmail.com>
2017-10-09 16:51:05 -0700

    feat: add dynamic release version from github

--- conftest.py
+++ conftest.py
@@ -1,7 +1,12 @@
 import os
 import pytest
+import requests
 
 import globals as gbl
+
+
+GITHUB_ORG = 'mozilla-services'
+GITHUB_REPO = 'autopush'
 
 
 @pytest.fixture(scope='session')
@@ -26,6 +31,22 @@ def ticket_num():
     gbl.ticket_num = '12345668'
 
 
+def get_tags(url):
+    """Get all tags as json from Github API."""
+
+    req = requests.get(url)
+    try:
+	if 'Not Found' in req.text:
+	    raise NotFoundError
+    except NotFoundError:
+	err_header = self.output.get_header('ERROR')
+	err_msg = '{0}\nNothing found at: \n{1}\nABORTING!\n\n'.format(
+	    err_header, url)
+	sys.exit(err_msg)
+    else:
+	return req
+
+
 @pytest.fixture(scope='session')
 def project_slug():
     return 'autopush-stage' 
@@ -34,4 +55,6 @@ def project_slug():
 @pytest.fixture(scope='session')
 def release_version():
     print('call github API')
-    return '1.37.0' 
+    url = 'https://api.github.com/repos/{0}/{1}/releases/latest'.format(GITHUB_ORG, GITHUB_REPO)
+    req = get_tags(url)
+    return req.json()['tag_name']
